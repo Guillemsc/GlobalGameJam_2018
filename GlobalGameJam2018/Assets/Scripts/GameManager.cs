bbe805a338d3f87eb5461607d389908600e6adc7
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2018-01-27 11:39:03 +0100

    Level manager

--- GlobalGameJam2018/Assets/Scripts/GameManager.cs
+++ GlobalGameJam2018/Assets/Scripts/GameManager.cs
@@ -4,21 +4,40 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private GameObject[] levels;
+    [SerializeField] private GameObject player;
+
+    GameObject curr_level = null;
+    GameObject curr_player = null;
 
     private void Awake()
     {
-        
+
     }
 
-    // Use this for initialization
-    void Start ()
+    public void LoadLevel(int level)
     {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-		
-	}
+        for (int i = 0; i < levels.Length; ++i)
+        {
+            Level curr = levels[i].GetComponent<Level>();
+
+            if(curr.GetLevelNum() == level)
+            {
+                if(curr_level != null)
+                {
+                    Destroy(curr_level);
+                }
+
+                if(player != null)
+                {
+                    Destroy(player);
+                }
+
+                curr_level = Instantiate(curr.gameObject, transform.position, Quaternion.identity);
+
+                Vector3 player_spawn = curr_level.GetComponent<Level>().GetSpawn().transform.position;
+                curr_player = Instantiate(player, player_spawn, Quaternion.identity);
+            }
+        }
+    }
 }
