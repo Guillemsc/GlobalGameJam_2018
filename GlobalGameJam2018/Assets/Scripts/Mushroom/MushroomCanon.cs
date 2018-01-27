81d711ddbf6e54720d7dda02e164eccbfe5634ed
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2018-01-27 01:54:07 +0100

    Working on canon

---
+++ GlobalGameJam2018/Assets/Scripts/Mushroom/MushroomCanon.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MushroomCanon : MonoBehaviour
+{
+    [SerializeField] float shoot_force;
+    [SerializeField] float suck_time;
+    [SerializeField] float rotation_time;
+
+
+}
---
+++ GlobalGameJam2018/Assets/Scripts/Utilities/Timer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Timer
+{
+    public void Start()
+    {
+        start_time = Time.timeSinceLevelLoad;
+    }
+
+    public float GetTime()
+    {
+        return Time.timeSinceLevelLoad - start_time;
+    }
+
+    float start_time = 0.0f;
+}
